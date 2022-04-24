f5a0ba7f5213edb4da480dc3ab967223cb0ab81f
Antonio Braz <antoniovbraz@gmail.com>
2022-04-24 13:47:57 -0300

    js to html integration

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let a = document.links[0]
 // Obtain the first link in the document
-
-a.href = 'globo.com'
+a.href = 'https://globo.com'
+a.target = '_blank'
+// a.setAttribute('href','https://globo.com')
 // set or change the destination URL of the selected link element
